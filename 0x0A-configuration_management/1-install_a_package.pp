11a4e82f5f7ca2264bde8ac56f97d3eeaa99b4c6
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-09-16 06:24:48 -0700

    updating 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # install flask from pip3
+package { 'pip3':
+  ensure   => 'installed'
+}
+
+exec { 'pip install flask':
+  command => '/usr/bin/pip3 install flask',
+}
+
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+  ensure => '2.1.0'
 }
