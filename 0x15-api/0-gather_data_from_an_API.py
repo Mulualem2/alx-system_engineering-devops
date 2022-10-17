a0c89ff76f58b5f434c1c2a09ea755eae3b2d003
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-10-17 02:10:33 -0700

    updating task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-"""Script that given employee ID, return information about his list progress"""
+'''
+For a given employee ID, returns information about his/her TODO list progress.
+'''
+
+import requests
+from sys import argv
 
 if __name__ == '__main__':
-    import requests
-    import sys
+    url_todo = 'https://jsonplaceholder.typicode.com/todos'
+    url_user = 'https://jsonplaceholder.typicode.com/users'
 
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
-    USER_ID = sys.argv[1]
-    req = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                       format(USER_ID)).json()
-    USERNAME = req.get("username")
-    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                       format(USER_ID)).json()
-    for item in req:
-        if item.get('completed') is True:
-            TASK_TITLE.append(item.get('title'))
-            NUMBER_OF_DONE_TASKS += 1
-    TOTAL_NUMBER_OF_TASKS = len(req)
+    response = requests.get(url_todo)
+    values = response.json()
+
+    name_get = requests.get(url_user)
+    user_name = name_get.json()
+    real_name = ""
+    for each_element in user_name:
+        if each_element.get('id') == int(argv[1]):
+            real_name = each_element.get('name')
+            break
+
+    full_list = []
+    for dict in values:
+        if dict.get('userId') == int(argv[1]):
+            full_list.append(dict)
+
+    true_elements = []
+    for completed in full_list:
+        if completed.get('completed'):
+            true_elements.append(completed)
 
     print('Employee {} is done with tasks({}/{}):'.
-          format(USERNAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for title in TASK_TITLE:
-        print('\t {}'.format(title))
+          format(real_name, len(true_elements), len(full_list)))
+    for task in true_elements:
+        print('\t {}'.format(task.get('title')))
