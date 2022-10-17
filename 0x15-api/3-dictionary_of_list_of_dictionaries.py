30523e19088b9a24c7a23c55fab4a53252f7add3
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-10-17 02:21:50 -0700

    updating task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,26 +4,28 @@ For a given employee ID, returns information about his/her
 TODO list progress in JSON format.
 '''
 
-import json
-import requests
-
-
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/users'
-    url_task = 'https://jsonplaceholder.typicode.com/user/id/todos'
-    users = requests.get(url).json()
-    filename = 'todo_all_employees.json'
-    with open(filename, mode='w') as file:
-        all_dict = {}
-        for user in users:
-            user_dict = {}
-            tasks_list = []
-            tasks = requests.get(url_task)
-            for task in tasks:
-                new_task_dict = {}
-                new_task_dict["username"] = user.get('username')
-                new_task_dict["task"] = task.get('title')
-                new_task_dict["completed"] = task.get('completed')
-                tasks_list.append(new_task_dict)
-            all_dict[user.get('id')] = tasks_list
-        json.dump(all_dict, file)
+    import json
+    import requests
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    json_return = {}
+    username_dict = {}
+
+    for user in users:
+        uid = user.get('id')
+        json_return[uid] = []
+        username_dict[uid] = user.get('username')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    for item in todos:
+        json_dictionary = {}
+        uid = item.get('userId')
+        json_dictionary['task'] = item.get('title')
+        json_dictionary['username'] = username_dict.get(uid)
+        json_dictionary['completed'] = item.get('completed')
+        json_return.get(uid).append(json_dictionary)
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(json_return, json_file)
