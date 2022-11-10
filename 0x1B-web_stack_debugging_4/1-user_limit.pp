190e1f54878028a7bce2081f273c73327718a87f
root <mulualemasfaw2321@gmail.com>
2022-11-09 23:48:16 -0800

    fixing bugs

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # User holberton
-exec {'hard-limit':
-    command  => 'sed -i "/holberton hard/s/5/10000" /etc/security/limits.conf',
-    provider => shell,
+exec { 'fixithard':
+  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
 
-exec {'soft-limit':
-    command  => 'sed -i "/holberton hard/s/4/10000" /etc/security/limits.conf',
-    provider => shell,
+exec { 'fixitsoft':
+  command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
